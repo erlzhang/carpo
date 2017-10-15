aef1a2f170f0e11dcf1afe6f17dc8c382789336b
zhangshiyu <741448394@qq.com>
2017-10-15 16:17:43 +0800

    完成book controller 基本增删改查操作

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :description, length: { maximum: 300 }
+  
+
   has_many :posts
   has_many :volumes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :books
+  end
+
+
+  get 'books/index'
+
+  get 'books/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
