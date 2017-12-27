7581859cdd076d0258418a78c5f85fc49e243327
zhangshiyu <741448394@qq.com>
2017-12-27 17:20:00 +0800

    dashboard

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -8,6 +8,12 @@ class ManageController < ApplicationController
       if @author.status == 2
         #dashboard没有内容，填完信息直接转调到书籍管理页面
         #redirect_to manage_books_path
+        @count = Hash.new 
+        books = @author.books
+        @count[:book] = books.size
+        @count[:post] = Post.where(:book_id => books).count
+        @count[:word] = Post.where(:book_id => books).sum("length(content)")
+        @count[:day] = ( Date.today - @author.created_at.to_date).to_i
       else
         redirect_to root_path
       end
