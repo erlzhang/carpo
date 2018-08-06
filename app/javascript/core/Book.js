8c741c6d620d63a4ec547ab9914873c96ec27ce6
zhangshiyu <741448394@qq.com>
2018-08-06 08:42:11 +0800

    搭建重构环境

---
+++ app/javascript/core/Book.js
@@ -0,0 +1,21 @@
+export default class Book {
+  constructor () {
+    this.id = ""
+    this.title = ""
+    this.description = ""
+    this.volumes = []
+    this.chapters = []
+    this.config = {}
+  }
+
+  create () {} 
+
+  update () {}
+
+  destroy () {}
+
+  sortPosts () {}
+
+  sortVolumes () {}
+
+}
---
+++ app/javascript/core/Volume.js
@@ -0,0 +1,14 @@
+export default class Volume {
+  constructor () {
+    this.title = ""
+    this.description = ""
+    this.chapters = []
+    this.book = null
+  }
+
+  create () {} 
+
+  update () {}
+
+  destroy () {}
+}
