4a3afbfdd421c994bf3c234fee5b2c0967f3630a
Sharon Zhang <zhangshiyu1992@hotmail.com>
2018-08-07 19:20:07 +0800

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module Leaf
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    config.time_zone = 'Beijing'
+    config.active_record.default_timezone = :local
 
     # 国际化
     # # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
